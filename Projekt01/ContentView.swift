102c25dd1426719f57e69cec66ad4cfe38eb4f6c
Andre Eggenschwiler <swissi1984@gmail.com>
2024-09-23 15:33:34 +0200

    Add LoginView and ProfilView

---
+++ Projekt01/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Projekt01
+//
+//  Created by Florian Rhein on 20.09.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Projekt01/Projekt01App.swift
@@ -0,0 +1,17 @@
+//
+//  Projekt01App.swift
+//  Projekt01
+//
+//  Created by Florian Rhein on 20.09.24.
+//
+
+import SwiftUI
+
+@main
+struct Projekt01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
